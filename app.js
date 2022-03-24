ec6274d1ca27972e9ed1d22c5ad535fc615b5395
Cody <alexhill318@yahoo.com>
2022-03-24 14:55:47 -0500

    added navbar color change onscroll

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
-let num = 3
-let otherNum = 4
+let header = document.querySelector('.header');
+let navbar = document.querySelector('.navbar')
 
-if (num && otherNum < 12) {
-    console.log(num + otherNum)
-}
+window.onscroll = () => {
+    if (window.scrollY > 100) {
+        header.classList.add('active');
+    } else {
+        header.classList.remove('active');
+    }
+};
